36c4200ac8aa5209b69b4ef8281dd687a9f95dc5
Josh Neil <joshneil8@googlemail.com>
2020-03-13 10:20:35 +0000

    Travis: pytype and pytest

--- convert.py
+++ convert.py
@@ -1,10 +1,11 @@
 import csv
-from anki_slides_converter import PDFToCardConverter, output_cards_to_csv_file
+from .anki_slides_converter import PDFDataExtractor, output_cards_to_csv_file
 
 if __name__ == "__main__":
     import sys
+
     with open(sys.argv[2], "w") as file:
         csv_file = csv.writer(file)
         pdf_filename = sys.argv[1]
-        pdf = PDFToCardConverter(pdf_filename)
+        pdf = PDFDataExtractor(pdf_filename)
         output_cards_to_csv_file(csv_file, pdf)
