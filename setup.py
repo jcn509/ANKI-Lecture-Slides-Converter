5753ed5d7ddefd39e23d1d717688e947cb7ad716
Josh Neil <joshneil8@googlemail.com>
2020-04-01 18:07:03 +0100

    pipenv-setup fix: this package shouldn't be listed in the Pipfile

--- setup.py
+++ setup.py
@@ -172,7 +172,9 @@ setup(
     # For example, the following would provide a command called `sample` which
     # executes the function `main` from this package when invoked:
     entry_points={
-        "console_scripts": ["anki_slides_converter=anki_slides_converter.command_line:main"]
+        "console_scripts": [
+            "anki_slides_converter=anki_slides_converter.command_line:main"
+        ]
     },  # Optional
     # List additional URLs that are relevant to your project as a dict.
     #
